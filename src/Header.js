2df47314267e359b1d05a92609d99a6b905621c5
jhall4345 <93298226+jhall4345@users.noreply.github.com>
2022-06-26 19:55:14 -0400

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Header.css";
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
+
+
+function Header() {
+  return (
+    <div className='header'>
+        <IconButton>
+            <PersonIcon fontSize="large" className='header_icon'/>
+        </IconButton>
+
+        <img
+        className='header_logo'
+        src='https://1000logos.net/wp-content/uploads/2018/07tinder-logo.png'
+        alt='tinderlogo'/>
+        <IconButton>
+            <ForumIcon fontSize="large" className='header_icon'/>
+        </IconButton>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/TinderCards.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./TinderCards.css"
+
+function TinderCards() {
+  return (
+    <div className='tindercards'>TinderCards</div>
+  )
+}
+
+export default TinderCards
